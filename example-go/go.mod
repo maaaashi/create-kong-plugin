e277aa92f286c74e5052916a87f7e5175d7919a6
maaaashi <doyaseizin.7@gmail.com>
2024-10-11 22:12:55 +0900

    update golang example

--- example-go/go.mod
+++
@@ -1,8 +0,0 @@
-module example-go
-
-go 1.22.0
-
-require (
-	github.com/Kong/go-pdk v0.11.0 // indirect
-	google.golang.org/protobuf v1.33.0 // indirect
-)
---
+++ example-go/src/main.go
@@ -0,0 +1,27 @@
+package main
+
+import (
+	"log"
+
+	"github.com/Kong/go-pdk"
+	"github.com/Kong/go-pdk/server"
+)
+
+func main() {
+	server.StartServer(New, Version, Priority)
+}
+
+var Version = "1.0.0"
+var Priority = 10
+
+type Config struct {
+	Message string
+}
+
+func New() interface{} {
+	return &Config{}
+}
+
+func (conf Config) Access(kong *pdk.PDK) {
+	log.Println("This is an example plugin handler, conf: ", conf)
+}
